5d631a86dff10f385268061139a33a40fcebb6ec
nerkulec <nerkulec@github.com>
2023-08-21 13:40:19 +0200

    CUDA OOM issue resolved

---
+++ examples/save/all_batches_vram_constant/mala/datahandling/graph.py
@@ -0,0 +1,154 @@
+import os
+import numpy as np
+
+import matplotlib.pyplot as plt
+from ase import Atoms
+from ase.io import read
+
+from tqdm.auto import trange
+
+import dgl
+import torch
+
+from functools import lru_cache
+from .utils import pickle_cache
+
+
+class HashableAtoms(Atoms):
+  def __hash__(self):
+    return hash(self.get_positions().tobytes())
+
+  def __eq__(self, other):
+    return np.allclose(self.get_positions(), other.get_positions())
+
+
+def get_ldos_positions(cell, nx=90, ny=90, nz=60):
+  ldos_positions = np.zeros((nx, ny, nz, 3), dtype=np.float32)
+  # I assume ldos values are evaluated at the center of each voxel
+  for x in range(nx):
+    ldos_positions[x, :, :, 0] = (x+0.5)/nx
+  for y in range(ny):
+    ldos_positions[:, y, :, 1] = (y+0.5)/ny
+  for z in range(nz):
+    ldos_positions[:, :, z, 2] = (z+0.5)/nz
+
+  ldos_positions = ldos_positions.reshape((-1, 3))
+  ldos_positions = cell.cartesian_positions(ldos_positions)
+  return ldos_positions
+
+
+def get_ldos(
+  ldos_path = "/bigdata/casus/wdm/Bartek_H2/H128/ldos/",
+  num_snapshots=20, nx=90, ny=90, nz=60, ldos_dim=201
+):
+  ldos = np.zeros((num_snapshots, nx, ny, nz, ldos_dim), dtype=np.float32)
+  
+  for i in trange(num_snapshots):
+    ldos[i] = np.load(os.path.join(ldos_path, f'H_snapshot{i}.out.npy'))
+  ldos = ldos.reshape((num_snapshots, -1, ldos_dim))
+  return ldos
+
+
+def repeat_cell(scaled_positions):
+  repeated_positions = [scaled_positions]
+  for dx in [-1, 0, 1]:
+    for dy in [-1, 0, 1]:
+      for dz in [-1, 0, 1]:
+        if dx == dy == dz == 0:
+          continue
+        
+        repeated_positions.append(scaled_positions + np.array([dx, dy, dz]))
+  repeated_positions = np.concatenate(repeated_positions)
+  return repeated_positions
+
+
+def prune_positions(all_positions, margin):
+  positions = all_positions
+  positions = positions[positions[:, 0] > -margin]
+  positions = positions[positions[:, 1] > -margin]
+  positions = positions[positions[:, 2] > -margin]
+  positions = positions[positions[:, 0] < 1+margin]
+  positions = positions[positions[:, 1] < 1+margin]
+  positions = positions[positions[:, 2] < 1+margin]
+  return positions
+
+
+def assign_relative_pos(graph, coords):
+  src, dst = graph.edges()
+  graph.edata['rel_pos'] = coords[src] - coords[dst]
+  graph.edata['rel_pos'] = graph.edata['rel_pos'].float()
+  return graph
+
+
+def get_periodic_graph(graph_repeated_positions, cartesian_positions, n_nodes=128):
+  graph_repeated_positions = assign_relative_pos(graph_repeated_positions, cartesian_positions)
+  src, dst = graph_repeated_positions.edges()
+  rel_pos = graph_repeated_positions.edata['rel_pos']
+  src = src % n_nodes
+  center_edges = dst < n_nodes
+  src = src[center_edges]
+  dst = dst[center_edges]
+  rel_pos = rel_pos[center_edges]
+  periodic_graph = dgl.graph((src, dst), num_nodes=n_nodes)
+  periodic_graph.edata['rel_pos'] = rel_pos
+  # periodic_graph.ndata['pos'] = cartesian_positions[:n_nodes]
+  return periodic_graph
+
+
+def get_ion_graph(filename, n_closest=8):
+  atoms = read(filename)
+  atoms.__class__ = HashableAtoms
+  cartesian_positions = atoms.get_positions()
+  cell = atoms.get_cell()
+  scaled_positions = cell.scaled_positions(cartesian_positions)
+  repeated_positions = repeat_cell(scaled_positions)
+  cartesian_positions = cell.cartesian_positions(repeated_positions)
+  cartesian_positions = torch.tensor(cartesian_positions, dtype=torch.float32)
+  graph_repeated_positions = dgl.knn_graph(cartesian_positions, n_closest)
+  periodic_graph = get_periodic_graph(graph_repeated_positions, cartesian_positions)
+  return periodic_graph
+
+# TODO: make the graphs uni-bipartite
+@lru_cache(maxsize=1000)
+@pickle_cache
+def get_ldos_graphs(
+  filename, ldos_batch_size=1000, n_closest_ldos=32, ldos_shape=(90, 90, 60, 201),
+  n_batches=None
+):
+  atoms = read(filename)
+  atoms.__class__ = HashableAtoms
+  cartesian_ion_positions = atoms.get_positions()
+  cell = atoms.get_cell()
+  cartesian_ldos_positions = get_ldos_positions(cell, *ldos_shape[:-1])
+  # ! TEMPORARY
+  if n_batches is not None:
+    cartesian_ldos_positions = cartesian_ldos_positions[:ldos_batch_size*n_batches]
+
+  cartesian_ldos_positions = torch.tensor(cartesian_ldos_positions, dtype=torch.float32)
+  n_ions = len(cartesian_ion_positions)
+  scaled_ion_positions = cell.scaled_positions(cartesian_ion_positions)
+  repeated_ion_positions = repeat_cell(scaled_ion_positions)
+  cartesian_ion_positions = cell.cartesian_positions(repeated_ion_positions)
+  cartesian_ion_positions = torch.tensor(cartesian_ion_positions, dtype=torch.float32)
+  ldos_graphs = []
+  for i in trange(0, len(cartesian_ldos_positions), ldos_batch_size, leave=False):
+    cartesian_ldos_positions_batch = cartesian_ldos_positions[i:i+ldos_batch_size]
+    distances = torch.cdist(cartesian_ldos_positions_batch, cartesian_ion_positions)
+    closest_ion_indices = torch.argsort(distances, dim=1)[:, :n_closest_ldos]
+    dst = torch.arange(ldos_batch_size).repeat_interleave(n_closest_ldos)
+    src = closest_ion_indices.flatten()
+    rel_pos = cartesian_ldos_positions_batch[dst] - cartesian_ion_positions[src]
+    src = src % n_ions
+    dst = dst + n_ions
+    # ldos_graph = dgl.DGLGraph()
+    # ldos_graph.add_nodes(n_ions, ntype='ion')
+    # ldos_graph.add_nodes(ldos_batch_size, ntype='grid')
+    # ldos_graph.add_edges(src, dst)
+    ldos_graph = dgl.graph((src, dst), num_nodes=n_ions+ldos_batch_size)
+
+    ldos_graph.edata['rel_pos'] = rel_pos
+    # ldos_graph.ndata['pos'] = torch.cat([cartesian_ion_positions[:n_ions], cartesian_ldos_positions_batch])
+    ldos_graphs.append(ldos_graph)
+  return ldos_graphs
+
+
---
+++ examples/save/all_batches_vram_constant/mala/network/runner_graph.py
@@ -0,0 +1,386 @@
+"""RunnerGraph class for running networks."""
+import os
+from zipfile import ZipFile, ZIP_STORED
+
+try:
+    import horovod.torch as hvd
+except ModuleNotFoundError:
+    # Warning is thrown by Parameters class
+    pass
+import numpy as np
+import torch
+
+from mala.common.parameters import ParametersRunning
+from mala.network.network import Network
+from mala.datahandling.data_scaler import DataScaler
+from mala.datahandling.data_handler import DataHandler
+from mala.datahandling.data_handler_graph import DataHandlerGraph
+from mala import Parameters
+
+from tqdm.auto import tqdm, trange
+
+
+class RunnerGraph:
+    """
+    Parent class for all classes that in some sense "run" the network.
+
+    That can be training, benchmarking, inference, etc.
+
+    Parameters
+    ----------
+    params : mala.common.parametes.Parameters
+        Parameters used to create this RunnerGraph object.
+
+    network : mala.network.network.Network
+        Network which is being run.
+
+    data : mala.datahandling.data_handler.DataHandler
+        DataHandler holding the data for the run.
+    """
+
+    def __init__(self, params, network, data, runner_dict=None):
+        self.parameters_full: Parameters = params
+        self.parameters: ParametersRunning = params.running
+        self.network = network
+        self.data = data
+        self.__prepare_to_run()
+
+    def save_run(self, run_name, save_path="./", zip_run=True,
+                 save_runner=False, additional_calculation_data=None):
+        """
+        Save the current run.
+
+        Parameters
+        ----------
+        run_name : str
+            Name under which the run should be saved.
+
+        save_path : str
+            Path where to which the run.
+
+        zip_run : bool
+            If True, the entire run will be saved as a .zip file. If False,
+            then the model will be saved as separate files.
+
+        save_runner : bool
+            If True, the RunnerGraph object will also be saved as object. This
+            is unnecessary for most use cases, but used internally when
+            a checkpoint is created during model training.
+
+        additional_calculation_data : string or bool
+            If this variable is a string, then additional calculation data will
+            be copied from the file this variable points to and included in
+            the saved model. If a a bool (and True), additional calculation
+            data will be saved directly from the DataHandler object.
+            Only has an effect in the .zip mode. If additional calculation
+            data is already present in the DataHandler object, it can be saved
+            by setting.
+        """
+        model_file = run_name + ".network.pth"
+        iscaler_file = run_name + ".iscaler.pkl"
+        oscaler_file = run_name + ".oscaler.pkl"
+        params_file = run_name + ".params.json"
+        if save_runner:
+            optimizer_file = run_name+".optimizer.pth"
+
+        self.parameters_full.save(os.path.join(save_path, params_file))
+        self.network.save_network(os.path.join(save_path, model_file))
+        self.data.input_data_scaler.save(os.path.join(save_path, iscaler_file))
+        self.data.output_data_scaler.save(os.path.join(save_path,
+                                                       oscaler_file))
+
+        files = [model_file, iscaler_file, oscaler_file, params_file]
+        if save_runner:
+            files += [optimizer_file]
+        if zip_run:
+            if additional_calculation_data is not None:
+                additional_calculation_file = run_name+".info.json"
+                if isinstance(additional_calculation_data, str):
+                    self.data.target_calculator.\
+                        read_additional_calculation_data(additional_calculation_data)
+                    self.data.target_calculator.\
+                        write_additional_calculation_data(os.path.join(save_path,
+                                                          additional_calculation_file))
+                elif isinstance(additional_calculation_data, bool):
+                    if additional_calculation_data:
+                        self.data.target_calculator. \
+                            write_additional_calculation_data(os.path.join(save_path,
+                                                              additional_calculation_file))
+
+                files.append(additional_calculation_file)
+            with ZipFile(os.path.join(save_path, run_name+".zip"), 'w',
+                         compression=ZIP_STORED) as zip_obj:
+                for file in files:
+                    zip_obj.write(os.path.join(save_path, file), file)
+                    os.remove(os.path.join(save_path, file))
+
+    @classmethod
+    def load_run(cls, run_name, path="./", zip_run=True,
+                 params_format="json", load_runner=True,
+                 prepare_data=False):
+        """
+        Load a run.
+
+        Parameters
+        ----------
+        run_name : str
+            Name under which the run is saved.
+
+        path : str
+            Path where the run is saved.
+
+        zip_run : bool
+            If True, MALA will attempt to load from a .zip file. If False,
+            then separate files will be attempted to be loaded.
+
+        params_format : str
+            Can be "json" or "pkl", depending on what was saved by the model.
+            Default is "json".
+
+        load_runner : bool
+            If True, a RunnerGraph object will be created/loaded for further use.
+
+        prepare_data : bool
+            If True, the data will be loaded into memory. This is needed when
+            continuing a model training.
+
+        Return
+        ------
+        loaded_params : mala.common.parameters.Parameters
+            The Parameters saved to file.
+
+        loaded_network : mala.network.network.Network
+            The network saved to file.
+
+        new_datahandler : mala.datahandling.data_handler.DataHandler
+            The data handler reconstructed from file.
+
+        new_runner : cls
+            (Optional) The runner reconstructed from file. For Tester and
+            Predictor class, this is just a newly instantiated object.
+        """
+        loaded_info = None
+        if zip_run is True:
+            loaded_network = run_name + ".network.pth"
+            loaded_iscaler = run_name + ".iscaler.pkl"
+            loaded_oscaler = run_name + ".oscaler.pkl"
+            loaded_params = run_name + ".params."+params_format
+            loaded_info = run_name + ".info.json"
+
+            zip_path = os.path.join(path, run_name + ".zip")
+            with ZipFile(zip_path, 'r') as zip_obj:
+                loaded_params = zip_obj.open(loaded_params)
+                loaded_network = zip_obj.open(loaded_network)
+                loaded_iscaler = zip_obj.open(loaded_iscaler)
+                loaded_oscaler = zip_obj.open(loaded_oscaler)
+                if loaded_info in zip_obj.namelist():
+                    loaded_info = zip_obj.open(loaded_info)
+                else:
+                    loaded_info = None
+
+        else:
+            loaded_network = os.path.join(path, run_name + ".network.pth")
+            loaded_iscaler = os.path.join(path, run_name + ".iscaler.pkl")
+            loaded_oscaler = os.path.join(path, run_name + ".oscaler.pkl")
+            loaded_params = os.path.join(path, run_name +
+                                         ".params."+params_format)
+
+        loaded_params = Parameters.load_from_json(loaded_params)
+        loaded_network = Network.load_from_file(loaded_params,
+                                                loaded_network)
+        loaded_iscaler = DataScaler.load_from_file(loaded_iscaler)
+        loaded_oscaler = DataScaler.load_from_file(loaded_oscaler)
+        new_datahandler = DataHandlerGraph(loaded_params,
+                                      input_data_scaler=loaded_iscaler,
+                                      output_data_scaler=loaded_oscaler,
+                                      clear_data=(not prepare_data))
+        if loaded_info is not None:
+            new_datahandler.target_calculator.\
+                read_additional_calculation_data(loaded_info,
+                                                 data_type="json")
+
+        if prepare_data:
+            new_datahandler.prepare_data(reparametrize_scaler=False)
+
+        if load_runner:
+            if zip_run is True:
+                with ZipFile(zip_path, 'r') as zip_obj:
+                    loaded_runner = run_name + ".optimizer.pth"
+                    if loaded_runner in zip_obj.namelist():
+                        loaded_runner = zip_obj.open(loaded_runner)
+            else:
+                loaded_runner = os.path.join(run_name + ".optimizer.pth")
+
+            loaded_runner = cls._load_from_run(loaded_params, loaded_network,
+                                               new_datahandler,
+                                               file=loaded_runner)
+            return loaded_params, loaded_network, new_datahandler, \
+                   loaded_runner
+        else:
+            return loaded_params, loaded_network, new_datahandler
+
+    @classmethod
+    def run_exists(cls, run_name, params_format="json", zip_run=True):
+        """
+        Check if a run exists.
+
+        Parameters
+        ----------
+        run_name : str
+            Name under which the run is saved.
+
+        zip_run : bool
+            If True, MALA will look for a .zip file. If False,
+            then separate files will be attempted to be loaded.
+
+        params_format : str
+            Can be "json" or "pkl", depending on what was saved by the model.
+            Default is "json".
+
+        Returns
+        -------
+        exists : bool
+            If True, the model exists.
+        """
+        if zip_run is True:
+            return os.path.isfile(run_name+".zip")
+        else:
+            network_name = run_name + ".network.pth"
+            iscaler_name = run_name + ".iscaler.pkl"
+            oscaler_name = run_name + ".oscaler.pkl"
+            param_name = run_name + ".params."+params_format
+            return all(map(os.path.isfile, [iscaler_name, oscaler_name, param_name,
+                                            network_name]))
+
+    @classmethod
+    def _load_from_run(cls, params, network, data, file=None):
+        # Simply create a new runner. If the child classes have to implement
+        # it theirselves.
+        loaded_runner = cls(params, network, data)
+        return loaded_runner
+
+    def _forward_entire_snapshot(self, snapshot_number, data_set,
+                                 data_set_type,
+                                 number_of_batches_per_snapshot=0,
+                                 batch_size=0):
+        """
+        Forward a snapshot through the network, get actual/predicted output.
+
+        Parameters
+        ----------
+        snapshot_number : int
+            Snapshot for which the prediction is done.
+            GLOBAL snapshot number, i.e. across the entire list.
+
+        number_of_batches_per_snapshot : int
+            Number of batches that lie within a snapshot.
+
+        batch_size : int
+            Batch size used for forward pass.
+
+        Returns
+        -------
+        actual_outputs : numpy.ndarray
+            Actual outputs for snapshot.
+
+        predicted_outputs : numpy.ndarray
+            Precicted outputs for snapshot.
+        """
+        grid_size = data_set.grid_size
+
+        # TODO: check if this makes sense
+        actual_outputs = np.zeros((grid_size, self.data.output_dimension))
+        embeddings = {}
+        for i in range(0, data_set.n_ldos_batches):
+            graph_ions, graph_grid = data_set[snapshot_number * data_set.n_ldos_batches + i]
+
+            targets = graph_grid.ndata['target']
+            targets_grid = targets[graph_ions.num_nodes():]
+            targets_grid = targets_grid.to('cpu')
+            targets_grid_scaled = self.data.output_data_scaler.inverse_transform(
+                targets_grid, as_numpy=True
+            )
+            i_start = i * data_set.ldos_batch_size
+            i_end = (i + 1) * data_set.ldos_batch_size
+            actual_outputs[i_start:i_end, :] = targets_grid_scaled
+
+
+            # actual_outputs[i * data_set.ldos_batch_size:(i + 1) * data_set.ldos_batch_size, :] = \
+            #     self.data.output_data_scaler.inverse_transform(
+            #         graph_grid.ndata['target'][graph_ions.num_nodes():].to('cpu'), as_numpy=True)
+
+        predicted_outputs = np.zeros((grid_size, self.data.output_dimension))
+        for i in trange(0, data_set.n_ldos_batches):
+            graph_ions, graph_grid = data_set[snapshot_number * data_set.n_ldos_batches + i]
+
+            graph_grid = graph_grid.to(
+                self.parameters._configuration["device"], non_blocking=True
+            )
+
+            graph_ions_hash = hash(graph_ions.edata['rel_pos'][:100].numpy().tobytes())
+            if graph_ions_hash not in embeddings:
+                torch.cuda.nvtx.range_push("embedding calcuation")
+                embedding_extended = self._compute_embedding(graph_ions, graph_grid)
+                torch.cuda.nvtx.range_pop()
+                embeddings[graph_ions_hash] = embedding_extended
+            embedding_extended = embeddings[graph_ions_hash]
+
+            predicted_outputs[i * data_set.ldos_batch_size:(i + 1) * data_set.ldos_batch_size, :] = \
+                self.data.output_data_scaler.inverse_transform(
+                    self.network.predict_ldos(embedding_extended, graph_ions, graph_grid).to('cpu'), as_numpy=True)
+
+            graph_grid.to('cpu', non_blocking=True)
+        
+        # Restricting the actual quantities to physical meaningful values,
+        # i.e. restricting the (L)DOS to positive values.
+        predicted_outputs = self.data.target_calculator.\
+            restrict_data(predicted_outputs)
+
+        # It could be that other operations will be happening with the data
+        # set, so it's best to reset it.
+        if self.data.parameters.use_lazy_loading:
+            data_set.return_outputs_directly = False
+
+        return actual_outputs, predicted_outputs
+
+    @staticmethod
+    def _correct_batch_size_for_testing(datasize, batchsize):
+        """
+        Get the correct batch size for testing.
+
+        For testing snapshot the batch size needs to be such that
+        data_per_snapshot / batch_size will result in an integer division
+        without any residual value.
+        """
+        # Does nothing
+        new_batch_size = batchsize
+        return new_batch_size
+
+    def _compute_embedding(self, graph_ions, graph_grid):
+        graph_ions = graph_ions.to(
+            self.parameters._configuration["device"], non_blocking=True
+        )
+        embedding = self.network.embed(graph_ions)
+        embedding_extended = self.network.extend_embedding(
+            embedding, graph_ions, graph_grid
+        )
+        graph_ions.to('cpu', non_blocking=True)
+        return embedding_extended
+
+    def __prepare_to_run(self):
+        """
+        Prepare the RunnerGraph to run the Network.
+
+        This includes e.g. horovod setup.
+        """
+        # See if we want to use horovod.
+        if self.parameters_full.use_horovod:
+            if self.parameters_full.use_gpu:
+                # We cannot use "printout" here because this is supposed
+                # to happen on every rank.
+                if self.parameters_full.verbosity >= 2:
+                    print("size=", hvd.size(), "global_rank=", hvd.rank(),
+                          "local_rank=", hvd.local_rank(), "device=",
+                          torch.cuda.get_device_name(hvd.local_rank()))
+                # pin GPU to local rank
+                torch.cuda.set_device(hvd.local_rank())
